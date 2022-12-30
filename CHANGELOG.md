c2e1894445fb81bc61334ad357b36a5c30c670eb
Sebastian Bauer <75776786+yax-lakam-tuun@users.noreply.github.com>
2022-12-30 11:45:51 +0000

    [#1] add release workflow to project

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to variant of [Calendar Versioning](https://calver.org/).
+
+Version scheme:
+
+The version of the document is basically the date of the release. 
+It is represented by the Maya Long Count notation of the release date 
+using the Martin-Skidmore correlation (584286).
+
+Every changelog entry consists of the version string and the Gregorian release date 
+using ISO format YYYY-MM-DD: 
+
+`[<long count of release date>] - <release date in ISO format>`
+
+
+## [13.0.10.2.18] - 2022-12-30
+
+### Added
+- Initial project setup
+- Support for development in docker container 
+- Main LaTeX file
+- Workflows for continuous integration (CI) and releases
+
---
+++ ahpula.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+####################################################################################################
+# ahpula.py  - A script to produce a long count date based from given date string
+#
+# Usage: ahpula.py [DATE]
+# 
+#        DATE
+#           The date has to be given in ISO format (e.g. 2022-12-30).
+#           If date is omitted, today's date is used instead.
+####################################################################################################
+
+import datetime
+import sys
+
+def digit_weight():
+    n = 1
+    while True:
+        yield n
+        n = 18 * n if n == 20 else 20 * n
+
+def maya_number(long_count):
+    weight = digit_weight()
+    return sum([next(weight) * i for i in long_count])
+
+def long_count_from_maya_number(maya_number):
+    weight = digit_weight()
+    next_weight = next(weight)
+    while maya_number != 0:
+        current_weight = next_weight
+        next_weight = next(weight)
+        lower_part = maya_number % next_weight
+        digit = lower_part // current_weight
+        maya_number = maya_number - lower_part
+        yield digit
+
+def long_count_from_date(date: datetime.datetime):
+    poco_uinic_eclipse_long_count = [16, 13, 19, 17, 9]
+    poco_uinic_eclipse_date = datetime.datetime(790, 7, 20) # julian date 16 July 790
+    poco_uinic_eclipse_maya_number = maya_number(poco_uinic_eclipse_long_count)
+    
+    delta = (date - poco_uinic_eclipse_date).days
+    date_maya_number = poco_uinic_eclipse_maya_number + delta
+    return long_count_from_maya_number(date_maya_number)
+
+def long_count_notation(long_count):
+    return '.'.join(reversed([str(i) for i in long_count]))
+
+gregorian_date = datetime.datetime.fromisoformat(sys.argv[1]) if len(sys.argv) == 2 else datetime.datetime.now()
+
+long_count = long_count_from_date(gregorian_date)
+version_string = long_count_notation(long_count)
+
+print(version_string)
