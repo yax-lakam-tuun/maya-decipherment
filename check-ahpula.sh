4932cf4079bc99700a4b67bc9d89dc89c1cc0f4b
Sebastian Bauer <75776786+yax-lakam-tuun@users.noreply.github.com>
2023-01-08 20:41:39 +0000

    [#15] Move ahpula checks into shell script

--- check-ahpula.sh
+++ check-ahpula.sh
@@ -3,10 +3,11 @@
 exit_code=0
 
 test_case() {
-    arguments=$1
-    nominal=$2
+    ahpula_path=$1
+    arguments=$2
+    nominal=$3
 
-    cmd="python3 ahpula.py $arguments"
+    cmd="python3 $ahpula_path $arguments"
     echo -n "Testing \"$cmd\"...."
     actual=$($cmd)
 
@@ -20,17 +21,20 @@ test_case() {
     fi
 }
 
-test_case "0790-07-20" "9.17.19.13.16"
-test_case "0790-07-20" "9.17.19.13.16"
-test_case "2022-12-30" "13.0.10.2.18"
-test_case "-cr 2022-12-30" "13.0.10.2.18 9 Etz'nab' 11 K'ank'in"
-test_case "-cr 2023-01-08" "13.0.10.2.18 9 Etz'nab' End of K'ank'in"
-test_case "--calendar-round 2022-12-30" "13.0.10.2.18 9 Etz'nab' 11 K'ank'in"
-test_case "--no-long-count -cr 2022-12-30" "9 Etz'nab' 11 K'ank'in"
-test_case "--tzolkin 2022-12-30" "13.0.10.2.18 9 Etz'nab'"
-test_case "--no-long-count --tzolkin 2022-12-30" "9 Etz'nab'"
-test_case "--haab 2022-12-30" "13.0.10.2.18 11 K'ank'in"
-test_case "--no-long-count --haab 2022-12-30" "11 K'ank'in"
+script_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ahpula_path="$script_path/ahpula.py"
+
+test_case "$ahpula_path" "0790-07-20" "9.17.19.13.16"
+test_case "$ahpula_path" "0790-07-20" "9.17.19.13.16"
+test_case "$ahpula_path" "2022-12-30" "13.0.10.2.18"
+test_case "$ahpula_path" "-cr 2022-12-30" "13.0.10.2.18 9 Etz'nab' 11 K'ank'in"
+test_case "$ahpula_path" "-cr 2023-01-08" "13.0.10.2.18 9 Etz'nab' End of K'ank'in"
+test_case "$ahpula_path" "--calendar-round 2022-12-30" "13.0.10.2.18 9 Etz'nab' 11 K'ank'in"
+test_case "$ahpula_path" "--no-long-count -cr 2022-12-30" "9 Etz'nab' 11 K'ank'in"
+test_case "$ahpula_path" "--tzolkin 2022-12-30" "13.0.10.2.18 9 Etz'nab'"
+test_case "$ahpula_path" "--no-long-count --tzolkin 2022-12-30" "9 Etz'nab'"
+test_case "$ahpula_path" "--haab 2022-12-30" "13.0.10.2.18 11 K'ank'in"
+test_case "$ahpula_path" "--no-long-count --haab 2022-12-30" "11 K'ank'in"
 
 if [ ! $exit_code ]; then
     echo "All tests passed!"
