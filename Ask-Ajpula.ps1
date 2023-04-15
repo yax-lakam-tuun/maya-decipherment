0dfb675933606b27e3c4a035fcd4815ec5087cb8
Sebastian Bauer <75776786+yax-lakam-tuun@users.noreply.github.com>
2023-04-15 12:16:01 +0000

    [#22] Implementing Tzolkin date

--- Ask-Ajpula.ps1
+++ Ask-Ajpula.ps1
@@ -11,24 +11,24 @@ param (
 class DistanceNumber {
     [int] $Days = 0
 
-    DistanceNumber([int] $days) {
-        $this.Days = days
+    DistanceNumber([int] $Days) {
+        $this.Days = $Days
     }
 }
 
 class MayaNumber {
     [int] $Days = 0
 
-    MayaNumber([int] $days) {
-        $this.Days = $days
+    MayaNumber([int] $Days) {
+        $this.Days = $Days
     }
 }
 
 class LongCountDate {
     [int[]] $Digits
 
-    LongCountDate([int[]] $digits) {
-        $this.Digits = $digits
+    LongCountDate([int[]] $Digits) {
+        $this.Digits = $Digits
     }
 
     LongCountDate([MayaNumber] $MayaNumber) {
@@ -65,6 +65,58 @@ class LongCountDate {
     }
 }
 
+enum TzolkinDayName {
+    Imix
+    Ik
+    Akbal
+    Kan
+    Chicchan
+    Kimi
+    Manik
+    Lamat
+    Muluk
+    Ok
+    Chuwen
+    Eb
+    Ben
+    Ix
+    Men
+    Kib
+    Kaban
+    Etznab
+    Kawak
+    Ajaw
+}
+
+function Get-StandardName() {
+    param (
+        [TzolkinDayName] $DayName
+    )
+
+    $StandardNames = (
+        "Imix", "Ik'", "Ak'b'al", "K'an",
+        "Chicchan", "Kimi", "Manik'", "Lamat",
+        "Muluk", "Ok", "Chuwen", "Eb'",
+        "B'en", "Ix", "Men", "Kib'",
+        "Kab'an", "Etz'nab'", "Kawak", "Ajaw"
+    )
+    return $StandardNames[$DayName]
+}
+
+class TzolkinDate {
+    [ValidateRange(1,13)][int] $TrecendaDay = 1
+    [TzolkinDayName] $DayName = [TzolkinDayName]::Imix
+
+    TzolkinDate( [int] $TrecendaDay, [TzolkinDayName] $DayName) {
+        $this.TrecendaDay = $TrecendaDay
+        $this.DayName = $DayName
+    }
+
+    [string] StandardNotation() {
+        return Get-StandardName($this.DayName)
+    }
+}
+
 $PocoUinicDate = [DateTime]::ParseExact("0790-07-20", "yyyy-MM-dd", $null) # julian date 16 July 790
 $Date = [DateTime]::ParseExact($IsoDate, "yyyy-MM-dd", $null)
 $Delta = NEW-TIMESPAN –Start $PocoUinicDate –End $Date
@@ -74,6 +126,11 @@ $lc = [LongCountDate]::new((16, 13, 19, 17, 9))
 $lc = [LongCountDate]::new([MayaNumber]::new(1412618))
 Write-Output $lc.StandardNotation()
 Write-Output $lc.MayaNumber()
+
+$t = [TzolkinDate]::new(0, [TzolkinDayName]::Ajaw)
+Write-Output $t
+Write-Output $(Get-StandardName([TzolkinDayName]::Akbal))
+
 
 #poco_uinic_eclipse_long_count = [16, 13, 19, 17, 9]
 #poco_uinic_eclipse_date = datetime.datetime(790, 7, 20) # julian date 16 July 790
