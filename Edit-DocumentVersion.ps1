2b5c52ed9e28528df5cbe9b0fa4c573ed5f753a0
Sebastian Bauer <75776786+yax-lakam-tuun@users.noreply.github.com>
2023-04-21 07:59:44 +0000

    [#22] Add some description

--- Edit-DocumentVersion.ps1
+++ Edit-DocumentVersion.ps1
@@ -1,27 +1,55 @@
 #!/usr/bin/env pwsh
 
+<#
+    .SYNOPSIS
+    Updates or prints content of given document version file.
+
+    .EXAMPLE
+    Update document version to anno 800 AD.
+
+    ./Edit-DocumentVersion.ps1 -Update -VersionTexPath ./document-version.tex -GregorianDate 0800-11-22
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    Print Long Count date of document version to terminal
+
+    ./Edit-DocumentVersion.ps1 -LongCount -VersionTexPath ./document-version.tex
+
+    .LINK
+    https://github.com/yax-lakam-tuun/maya-decipherment
+#>
+
 [CmdletBinding()]
 param (
     [Parameter(Mandatory=$true, ParameterSetName="UpdateSet")]
-    [Parameter(Mandatory=$true, ParameterSetName="PrintSet")]
+    [Parameter(Mandatory=$true, ParameterSetName="LongCountSet")]
+    # Path to LaTeX file in which the version is stored/should be stored in.
     [string] 
     $VersionTexPath,
 
     [Parameter(Mandatory=$true, ParameterSetName="UpdateSet")]
+    # Creates a new document version in LaTeX format and writes it to given Tex file.
     [switch]
     $Update,
 
-    [Parameter(Mandatory=$true, ParameterSetName="PrintSet")]
+    [Parameter(Mandatory=$true, ParameterSetName="LongCountSet")]
+    # Prints Long Count portion stored in given Tex file.
     [switch]
-    $Print,
+    $LongCount,
 
     [Parameter(Mandatory=$true, ParameterSetName="UpdateSet")]
     [ValidatePattern("\d{4}-\d{2}-\d{2}")]
+    # Gregorian date in ISO format.
     [string]
     $GregorianDate
 )
 
-function Write-DocumentVersion {
+function Write-LongCountDate {
     [CmdletBinding()]
     param (
         [string] $VersionTexPath
@@ -48,8 +76,8 @@ function Update-DocumentVersion {
     ./Ask-Ajpula.ps1 -Latex -GregorianDate $GregorianDate | Out-File $VersionTexPath
 }
 
-if ($Print.IsPresent) {
-    Write-DocumentVersion -VersionTexPath $VersionTexPath
+if ($LongCount.IsPresent) {
+    Write-LongCountDate -VersionTexPath $VersionTexPath
 }
 
 if ($Update.IsPresent) {
